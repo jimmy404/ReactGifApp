6b77a1b9e0778faece4e086a02a33a5ca6e86935
Jimmy200 <bruno_pier@hotmail.com>
2021-06-23 19:51:47 -0300

    Imports GifGridItem into GifGrid

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
+import { GifGridItem } from "./GifGridItem";
 
 export const GifGrid = ({ category }) => {
+  const [images, setImages] = useState([]);
+
   useEffect(() => {
     getGifs();
   }, []);
@@ -18,11 +21,15 @@ export const GifGrid = ({ category }) => {
       };
     });
     console.log(gifs);
+    setImages(gifs);
   };
 
   return (
     <div>
       <h3>{category}</h3>
+      {images.map((img) => (
+        <GifGridItem key={img.id} {...img} />
+      ))}
     </div>
   );
 };
