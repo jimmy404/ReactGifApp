dd8303e08099fe5ff7dd18bf639617221f2b05a1
Jimmy200 <bruno_pier@hotmail.com>
2021-06-23 19:10:04 -0300

    Adds GifGrid component and fetch basic data

---
+++ src/components/GifGrid.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+export const GifGrid = ({ category }) => {
+  const getGifs = async () => {
+    const url =
+      "https://api.giphy.com/v1/gifs/search?q=evangelion&limit=15&api_key=UiHvQwmEAGOZk1fLzAzBXKJnRaqgQPRE";
+    const resp = await fetch(url);
+    const { data } = await resp.json();
+    const gifs = data.map((img) => {
+      return {
+        id: img.id,
+        title: img.title,
+        url: img.images?.downsized_medium.url,
+      };
+    });
+    console.log(gifs);
+  };
+
+  getGifs();
+
+  return (
+    <div>
+      <h3>{category}</h3>
+    </div>
+  );
+};
