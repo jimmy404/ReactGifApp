9d4697998151eccaf24aaa1f3a0d29d929d7eb64
Jimmy200 <bruno_pier@hotmail.com>
2021-06-23 19:15:39 -0300

    Adds useEffect in fetch call

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export const GifGrid = ({ category }) => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    getGifs();
+  }, []);
+
   const getGifs = async () => {
     const url =
       "https://api.giphy.com/v1/gifs/search?q=evangelion&limit=15&api_key=UiHvQwmEAGOZk1fLzAzBXKJnRaqgQPRE";
@@ -16,11 +22,11 @@ export const GifGrid = ({ category }) => {
     console.log(gifs);
   };
 
-  getGifs();
-
   return (
     <div>
       <h3>{category}</h3>
+      <h3>{count}</h3>
+      <button onClick={() => setCount(count + 1)}></button>
     </div>
   );
 };
