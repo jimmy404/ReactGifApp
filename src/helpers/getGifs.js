47aacdce27e376c7109e12447c3751f9dd86e862
Jimmy200 <bruno_pier@hotmail.com>
2021-06-24 20:37:13 -0300

    Adds new limit for fetch

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,7 +1,7 @@
 export const getGifs = async (category) => {
   const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(
     category
-  )}&limit=15&api_key=UiHvQwmEAGOZk1fLzAzBXKJnRaqgQPRE`;
+  )}&limit=21&api_key=UiHvQwmEAGOZk1fLzAzBXKJnRaqgQPRE`;
   const resp = await fetch(url);
   const { data } = await resp.json();
   const gifs = data.map((img) => {
